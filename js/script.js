a8939f0730a3d8f3b920d4957ffa4eda1e7f56dd
smilesandtea <30906377+smilesandtea@users.noreply.github.com>
2019-05-29 21:43:09 -0400

    Updated script.js -- testing something

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ let randomNum2 = Math.floor(Math.random() * 12 + 1);
 
 
 // Make the random numbers filter through images for both players
-document.querySelector('.img-one').setAttribute('src', `../img/pokemon${randomNum1}.png`);
-document.querySelector('.img-two').setAttribute('src', `../img/pokemon${randomNum2}.png`);
+document.querySelector('.img-one').setAttribute('src', `img/pokemon${randomNum1}.png`);
+document.querySelector('.img-two').setAttribute('src', `img/pokemon${randomNum2}.png`);
 
 
 // Conditionals to determine which Pokemon wins based on highest random number
