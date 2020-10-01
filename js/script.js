6e7982879fb6dda16eb79828a25b1f1879d601d6
shadesofpixie <30906377+shadesofpixie@users.noreply.github.com>
2020-10-01 12:09:57 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Create two random number variables to cycle through Pokemon
 let randomNum1 = Math.floor(Math.random() * 12 + 1);
-let randomNum2 = Math.floor(Math.random() * 12 + 1);
+
 
 
 // Make the random numbers filter through images for both players
